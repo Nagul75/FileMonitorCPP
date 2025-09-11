2adad96fdaa647c776501840140b62c76f8c2734
Nagul75 <nagul697@gmail.com>
2025-09-11 13:58:46 +0530

    Add factory function - return based on platform

--- Factory.cpp
+++ Factory.cpp
@@ -1,3 +1,16 @@
 //
 // Created by regal on 9/11/25.
 //
+
+#include "FileMonitor.h"
+
+#ifdef __linux__
+#include "FileMonitorLinux.h"
+#endif
+
+std::unique_ptr<IFileMonitor> createFileMonitor()
+{
+#ifdef __linux__
+    return std::make_unique<FileMonitorLinux>();
+#endif
+}
