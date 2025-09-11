1b438ed5aed8c2180aac77555a84b367f1f5900b
Nagul75 <nagul697@gmail.com>
2025-09-11 13:20:15 +0530

    Set up file monitor interface class

--- FileMonitor.h
+++ FileMonitor.h
@@ -5,4 +5,18 @@
 #ifndef FILEMONITOR_FILEMONITOR_H
 #define FILEMONITOR_FILEMONITOR_H
 
+#include <memory>
+
+class IFileMonitor
+{
+protected:
+    virtual ~IFileMonitor() = default;
+    virtual bool addWatch(const std::string& path) = 0;
+    virtual bool removeWatch(const std::string& path) = 0;
+    virtual void start() = 0;
+    virtual void stop() = 0;
+};
+
+std::unique_ptr<IFileMonitor> createFileMonitor();
+
 #endif //FILEMONITOR_FILEMONITOR_H
